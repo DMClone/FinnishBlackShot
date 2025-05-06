fce71073a7ea8b804256419d2485f29c96d40a80
Max Bos <maxbos230100@gmail.com>
2025-05-06 13:46:13 +0200

    playermanager added

--- Assets/Scripts/Max/ActivateDisplays.cs
+++ Assets/Scripts/Max/ActivateDisplays.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class ActivateDisplays : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        SetUpDisplays();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetUpDisplays()
     {
-        
+        Debug.Log("Displays connected: " + Display.displays.Length);
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
     }
 }

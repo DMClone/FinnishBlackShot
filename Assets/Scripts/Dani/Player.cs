4aa3a7c4242703675f9b0e8e30f6ecb54b22c9c8
DMClone <230381@student.glu.nl>
2025-05-07 12:10:32 +0200

    Player input

---
+++ Assets/Scripts/Dani/Player.cs
@@ -0,0 +1,26 @@
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public PlayerInput _playerInput;
+
+    private void Awake()
+    {
+        InputAction PlayerPlay = InputSystem.actions.FindAction("Play");
+        PlayerPlay.performed += ActionOne;
+        InputAction PlayerPass = InputSystem.actions.FindAction("Pass");
+        PlayerPass.performed += ActionOne;
+    }
+
+    public void ActionOne(InputAction.CallbackContext context)
+    {
+        // Do action
+    }
+
+    public void ActionTwo(InputAction.CallbackContext context)
+    {
+        // Do action
+    }
+}

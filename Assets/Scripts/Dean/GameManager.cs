57af7c684f67848101fc3046ea3b43e3520b948f
Max Bos <maxbos230100@gmail.com>
2025-05-09 09:57:14 +0200

    merge fixed

--- Assets/Scripts/Dean/GameManager.cs
+++ Assets/Scripts/Dean/GameManager.cs
@@ -23,11 +23,8 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private PlayerManager playerManager;
 
-<<<<<<< Updated upstream
     [SerializeField] private AudioSource audioSource;
 
-=======
->>>>>>> Stashed changes
     private void Awake()
     {
         // Singleton pattern implementation
